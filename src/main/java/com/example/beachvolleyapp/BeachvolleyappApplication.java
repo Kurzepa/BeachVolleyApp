a6bf72e5586ad61edfe7a618823b567f7c430fbb
Unknown <218436@student.pwr.edu.pl>
2018-10-16 13:53:19 +0200

    utworzenie projektu

---
+++ src/main/java/com/example/beachvolleyapp/BeachvolleyappApplication.java
@@ -0,0 +1,12 @@
+package com.example.beachvolleyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachvolleyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeachvolleyappApplication.class, args);
+    }
+}
